8cea3a130285baedd0546bf61dca3ce3bb4909ad
Saravanakumar Murugesan <saravanakumar.murugesan.ext@nokia.com>
2023-02-17 15:11:42 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -21,35 +21,35 @@ def blogs():
     return render_template('works.html')
 
 
-# def write_to_file(data):
-#     with open('database.txt', mode='a') as database:
-#         email = data["email"]
-#         subject = data["subject"]
-#         message = data["message"]
-#         file = database.write(f'\n{email},{subject},{message}')
-#
-#
-# def write_to_csv(data):
-#     with open('database.csv', newline='', mode='a') as database2:
-#         email = data["email"]
-#         subject = data["subject"]
-#         message = data["message"]
-#         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#         csv_writer.writerow([email, subject, message])
-#
-#
-# @app.route('/submit_form', methods=['POST', 'GET'])
-# def submit_form():
-#     if request.method == 'POST':
-#         try:
-#             data = request.form.to_dict()
-#             write_to_csv(data)
-#             return redirect('/thankyou.html')
-#         except Exception as e:
-#             err = e
-#             return 'did not save to database'
-#     else:
-#         return 'something went wrong. Try again!'
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except Exception as e:
+            err = e
+            return 'did not save to database'
+    else:
+        return 'something went wrong. Try again!'
 
 
 if __name__ == '__main__':
